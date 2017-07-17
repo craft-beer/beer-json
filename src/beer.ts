c073916a4a29f81ff5d25d3eed7804b0b5e6fb69
Gim YoungPhil <youngphil.gim@gmail.com>
2017-07-17 23:49:28 +0900

    first init

---
+++ src/beer.ts
@@ -0,0 +1,28 @@
+export interface Beer {
+  // 바코드
+  code: string,
+  // 제품명
+  product: string,
+  // 원산지
+  country?: string,
+  // 제조회사
+  company?: string,
+  // 수입원 및 판매원
+  importation?: string,
+  // 원재료명
+  ingredients?: Array<string>,
+  // 알코올 함량
+  alcohol?: number,
+  // 용량
+  volume?: number,
+  // 카테고리
+  categories?: Array<string>,
+  // 컬러
+  rgb?: string,
+  // 가격
+  price?: Array<{ place: string, data: Date, price: number }>
+  // 리뷰
+  reviews?: Array<string>,
+  // 이미지
+  images?: Array<string>,
+}
